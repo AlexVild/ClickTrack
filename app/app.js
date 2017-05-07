2f3eb2a2dd2c5a6d514b3f5d6528bd08b9f415b9
AlexVild <ajv2324@gmail.com>
2017-05-07 17:39:41 -0400

    Metronome implemented

---
+++ app/app.js
@@ -0,0 +1,52 @@
+'use strict'
+
+angular.module('CTApp', [])
+.controller('homeController', function($scope, $interval, $timeout){
+    $scope.bpm = 120;
+    $scope.perMeasure = 4;
+    $scope.noteValues = [4, 8];
+    $scope.noteValue = 4;
+    $scope.playMet = false;
+    var metPos = 0;
+    var accent = new Audio('src/accent.wav');
+    var beat = new Audio('src/beat.wav');
+
+    var getMetTime = function(){
+        if($scope.noteValue == 8){
+            return ((60/$scope.bpm) * 1000) / 2;
+        } else{
+            return (60/$scope.bpm) * 1000;
+        }
+    }
+
+    var playMetInterval = function(){
+        if(metPos >= $scope.perMeasure) metPos = 0;
+        if(metPos === 0){
+            accent.play();
+        } else{
+            beat.play();
+        }
+        metPos++;
+    };
+    var metSample = $interval(playMetInterval, getMetTime());
+
+    $scope.toggleMet = function(){
+        metPos = 0;
+        $scope.playMet = !$scope.playMet;
+    }
+
+    $scope.stopMet = function(){
+        $scope.playMet = false;
+    }
+
+    $scope.$watch('playMet', function(newV){
+        if(newV === false){
+            $interval.cancel(metSample);
+        } else{
+            playMetInterval();
+            $timeout(function () {
+                metSample = $interval(playMetInterval, getMetTime());
+            }, getMetTime);
+        }
+    });
+});
